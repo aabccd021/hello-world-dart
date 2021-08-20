21d22b6dee5555a80066e958f7c8b49dbe38b1c2
aabccd021 <aabccd021@gmail.com>
2021-08-20 10:56:08 +0700

    check

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,67 @@
-import 'mainmain.dart';
+abstract class E<L, R> {
+  const E();
 
-abstract class Either<L, R> {
-  const Either();
-  B match<B>(B ifLeft(Left<L, R> l), B ifRight(Right<L, R> r));
+  factory E.left(L left) => Left(left, new Error());
+  factory E._left(L left, [Error? e]) => Left(left, e ?? new Error());
+  factory E.right(R right) => Right(right);
+
+  ///
+  B Ematch<B>(B ifLeft(Left<L, R> l), B ifRight(Right<L, R> r));
+
+  ///
+  E<L, RResult> Emap<RResult>(RResult Function(R r) f) =>
+      Ematch((l) => E._left(l.left, l.errObj), (r) => E.right(f(r.right)));
+
+  ///
+  E<LResult, R> EmapLeft<LResult>(LResult Function(L r) f) =>
+      Ematch((l) => E._left(f(l.left), l.errObj), (r) => E.right(r.right));
+
+  ///
+  T Efold<T>(T Function(L l) onLeft, T Function(R r) onRight) =>
+      Ematch((l) => onLeft(l.left), (r) => onRight(r.right));
+
+  ///
+  R EgetOrElse(R Function(L l) f) => Efold(f, (r) => r);
+
+  ///
+  E<L, RResult> Echain<RResult>(E<L, RResult> Function(R r) f) =>
+      Emap(f).Ematch((l) => E._left(l.left, l.errObj), (r) => r.right);
+
+  ///
+  E<L, RResult> Echain2<RResult>(E<L, E<L, RResult>> Function(R r) f) =>
+      Emap(f).Echain(
+          (r) => r.Ematch((l) => E._left(l.left, l.errObj), (r) => r.right));
 }
 
-class Right<L, R> extends Either<L, R> {
+class Right<L, R> extends E<L, R> {
   final R right;
   const Right(this.right);
-
   @override
-  B match<B>(
+  B Ematch<B>(
           B Function(Left<L, R> l) ifLeft, B Function(Right<L, R> r) ifRight) =>
       ifRight(this);
+  @override
+  int get hashCode => right.hashCode;
 }
 
-class Left<L, R> extends Either<L, R> {
+class Left<L, R> extends E<L, R> {
   final L left;
-  const Left(this.left);
-
+  final Error errObj;
+  Left(this.left, this.errObj);
   @override
-  B match<B>(
+  B Ematch<B>(
           B Function(Left<L, R> l) ifLeft, B Function(Right<L, R> r) ifRight) =>
       ifLeft(this);
-}
-
-Either<L, R> left<L, R>(L left) {
-  return Left(left);
-}
-
-Right<L, R> right<L, R>(R right) {
-  return Right(right);
-}
-
-typedef Fn<L, R, T> = T Function(Either<L, R>);
-
-Fn<L, R, T> match<L, R, T>(
-    T Function(Left<L, R> l) ifLeft, T Function(Right<L, R> r) ifRight) {
-  return (e) => e.match(ifLeft, ifRight);
-}
-
-Fn<L, R, Either<L, RResult>> map<L, R, RResult>(RResult Function(R r) f) {
-  return match((l) => left(l.left), (r) => right(f(r.right)));
-}
-
-Fn<L, R, T> fold<L, R, T>(T Function(L l) onLeft, T Function(R r) onRight) {
-  return match((l) => onLeft(l.left), (r) => onRight(r.right));
+  @override
+  int get hashCode => left.hashCode;
 }
 
 main() {
-  final lefted = left("yaa");
-  final righted = right(46);
+  final lefted = E.left("yaa");
+  final righted = E.right(46);
 
-  print(pipe(lefted)
-      .p(map((x) => 'map' + x.toString()))
-      .p(fold((l) => 'left' + l, (r) => r + 'right'))
-      // .p(match((l) => 'left' + l.toString(), (r) => 'right' + r.toString()))
-      .v());
-  print(pipe(righted)
-      .p(map((x) => 'mapright2' + x.toString()))
-      .p(fold((l) => 'left' + l, (r) => r + 'right'))
-      // .p(match((l) => 'left' + l.toString(), (r) => 'right' + r.toString()))
-      .v());
+  final x = lefted.Emap((r) => 'map ' + r)
+      .Efold((l) => 'left ' + l, (r) => 'right ' + r.toString());
 
-  // print(lefted.match((l) => 'left ' + l, (r) => 'right ' + r.toString()));
-  // print(righted.match((l) => 'left ' + l, (r) => 'right ' + r.toString()));
+  print(x);
 }
